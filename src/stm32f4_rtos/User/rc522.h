e733489e284a6483ce5330671ed8f430a28bb119
15091282640 <605877125@qq.com>
2019-01-04 00:14:34 +0800

    1、RFID刷卡时会硬件错误

--- src/stm32f4_rtos/User/rc522.h
+++ src/stm32f4_rtos/User/rc522.h
@@ -10,7 +10,17 @@
 #define COLLISION_GRADE_2       0x95 
 #define COLLISION_GRADE_3       0x97 
 
+#define Mifare1_S50     0x0004
+#define Mifare1_S70     0x0002
+#define Mifare1_Light     0x0010
+
+extern u8 rfid_rev_flag;
+extern u8 rfid_rev_buf[20];
+extern u8 rfid_rev_cnt;
+
 void rc522_cmd_request(u8 mode);
 void rc522_cmd_anticoll(u8 cmd);
 void rc522_cmd_select(u8 *uid);
+u8 rc522_find(u8 *buf,u8 len);
+void rc522_card_id(u8 *buf,u8 len,u8 *card);
 #endif
